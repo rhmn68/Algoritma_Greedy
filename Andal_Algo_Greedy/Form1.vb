1754ab4a6471544c64eee79fa41688636aab4a6d
rhmn68 <auliar68@gmail.com>
2017-05-06 05:44:48 +0700

    Add files via upload

---
+++ Andal_Algo_Greedy/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim nilai As String = TextBox1.Text
+        Dim nilai1 As Integer = Mid(nilai, 1, 1)
+        Dim nilai2 As Integer = Mid(nilai, 3, 1)
+        Dim nilai3 As Integer = Mid(nilai, 5, 1)
+        Dim tukar As Integer = CStr(TextBox2.Text)
+        Dim hasil As String
+        hasil = ""
+        While tukar > 0
+            If (tukar - nilai3) > 0 Then
+                tukar = tukar - nilai3
+                hasil = hasil & CStr(nilai3)
+                If (tukar + nilai3) > nilai3 Then
+                    hasil = hasil & "+"
+                End If
+            ElseIf (tukar - nilai2) > 0 Then
+                tukar = tukar - nilai2
+                hasil = hasil & CStr(nilai2)
+                If (tukar + nilai2) > nilai2 Then
+                    hasil = hasil & "+"
+                End If
+            ElseIf (tukar - nilai1) >= 0 Then
+                tukar = tukar - nilai1
+                hasil = hasil & CStr(nilai1)
+                If (tukar + nilai1) > nilai1 Then
+                    hasil = hasil & "+"
+                End If
+            End If
+        End While
+        TextBox3.Text = hasil
+    End Sub
+End Class
+
---
+++ Andal_Algo_Greedy/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Andal_Algo_Greedy")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Andal_Algo_Greedy")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0aca1f39-40aa-4789-8d3a-4854ed708099")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
